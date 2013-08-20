ba510c35835001441035631f396c0c6322d51406
root <root@satellite.(none)>
2013-08-20 14:51:51 +0000

    Log class update

--- classes/log.class.php
+++ classes/log.class.php
@@ -2,38 +2,63 @@
 
 namespace baobab;
 
-class Log {
+class LogLevel {
 
-    const NONE = 0;    
+    const NONE = 0;
     const ERROR = 1;
     const WARN = 2;
     const INFO = 3;
     const DEBUG = 4;
     const ALL = 100;
-    
-    public static $level = self::NONE;
+
+    public static $enums= array(
+        self::NONE => "none",
+        self::ERROR => "error",
+        self::WARN => "warn",
+        self::INFO => "info",
+        self::DEBUG => "debug",
+        self::ALL => "all",
+    );
+
+    public static function toString($enum) {
+        return self::$enums[$enum];
+    }
+}
+
+class Log {
+
+    private static $_level = LogLevel::ALL;
 
     public static function error($message) {
-        if(self::$level >= self::ERROR ) {
+        if(self::$_level >= LogLevel::ERROR ) {
             error_log("ERROR: ". $message);
         }
     }
     
     public static function warn($message) {
-        if(self::$level > self::WARN ){
+        if(self::$_level > LogLevel::WARN ){
             error_log("WARN: " . $message);
         }
     }
     
     public static function info($message) {
-        if( self::$level > self::INFO ){
+        if( self::$_level > LogLevel::INFO ){
             error_log("INFO: ". $message);
         }
     }
     
     public static function debug($message) {
-        if( self::$level > self::DEBUG ){
+        if( self::$_level > LogLevel::DEBUG ){
             error_log("DEBUG: ". print_r($message, true));
         }
     }
+
+    public static function setLevel( $level) {
+        self::$_level = $level;
+    }
+
+    public static function getLevel() {
+        return LogLevel::toString(self::$_level);
+    }
+
 }
